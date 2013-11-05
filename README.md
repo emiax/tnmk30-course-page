8b42d0e79b11e83a322e71b8206dc1aad1c8ea57
J-Zeitler <jonze168@student.liu.se>
2013-11-05 15:20:53 +0100

    updated some content and readme with deployment info

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ tnmk30-course-page
 ==================
 
 The course web page for TNMK30, "Elektronisk Publicering" at ITN, Linköping University
+
+###Deployment:
+1. Change `rootUrl` in `config.json` to the website root
+2. Change absolute path for the `RewriteEngine` in `.htaccess` to:
+    
+        /<website root>/index.php?page=$0
--- navigation.php
+++ navigation.php
@@ -6,7 +6,7 @@
    <span class="icon-bar"></span>
    <span class="icon-bar"></span>
    </button>
-   <a class="navbar-brand" href="<?php echo $config["rootUrl"]; ?>">TNMK30</a>
+   <a class="navbar-brand" href="<?php echo $config['rootUrl']; ?>">TNMK30</a>
    </div>
    
    <div class="collapse navbar-collapse navbar-ex1-collapse">
